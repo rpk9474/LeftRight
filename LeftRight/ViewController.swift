8faeb8332e43c4fdbd0080cfa64b6f64dd14ba6d
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-13 15:46:20 +0900

    LEFT RIGHT Button

--- LeftRight/ViewController.swift
+++ LeftRight/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ButtonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func LBT(_ sender: Any) {
+    
+        ButtonLabel.text = "LEFT Button Clicked"
+    }
+    
+    @IBAction func RBT(_ sender: Any) {
+        ButtonLabel.text = "RIGHT Button Clicked"
+    }
+    
 }
 
